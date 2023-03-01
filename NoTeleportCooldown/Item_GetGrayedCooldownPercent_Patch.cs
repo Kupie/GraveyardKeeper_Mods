9d0d6a894f7030e19c52392b5fc1dffa964a7c4f
Kupie <kupokraft@gmail.com>
2023-02-28 23:00:50 -0600

    Add files via upload

---
+++ NoTeleportCooldown/Item_GetGrayedCooldownPercent_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace NoTeleportCooldown;
+
+
+[HarmonyPatch(typeof(Item), nameof(Item.GetGrayedCooldownPercent))]
+class HarmonyPatch_IgnoreCooldownTeleportStone
+{
+    static bool Prefix(Item __instance, ref int __result)
+    {
+        //Harmony.DEBUG = true;
+        //FileLog.Log(__instance.ToString());
+        //Harmony.DEBUG = false;
+        if (__instance.id == "hearthstone")
+        {
+            __result = 0;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
---
+++ NoTeleportCooldown/MainPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace NoTeleportCooldown;
+public static class MainPatcher
+{
+    public static void Patch()
+    {
+        var harmony = new Harmony("org.Kupie.NoTeleportCooldown");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
+
+
+
