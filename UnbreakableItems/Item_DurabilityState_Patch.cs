9cad9e1a670fcde73e865d49c9bec6395862bda6
Kupie <kupokraft@gmail.com>
2023-03-03 10:59:04 -0600

    Add files via upload

---
+++ UnbreakableItems/Item_DurabilityState_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace UnbreakableItems;
+
+
+[HarmonyPatch(typeof(HPActionComponent), nameof(HPActionComponent.DoAction))]
+class HarmonyPatch_DurabilityState
+{
+    static void Prefix(WorldGameObject player_wgo)
+    {
+        Item equippedTool = player_wgo.GetEquippedTool();
+        if (equippedTool.definition.durability_decrease_on_use)
+        {
+            equippedTool.definition.durability_decrease_on_use = false;
+        }
+
+    }
+}
---
+++ UnbreakableItems/MainPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace UnbreakableItems;
+public static class MainPatcher
+{
+    public static void Patch()
+    {
+        var harmony = new Harmony("org.Kupie.UnbreakableItems");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
+
+
+
