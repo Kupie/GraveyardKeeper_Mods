2772ae62c09553ec76910763b357318e48eca26b
Kupie <kupokraft@gmail.com>
2023-03-04 21:11:43 -0600

    Add files via upload

---
+++ MuteOnUnfocus/MuteOnUnfocus_Patch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UnityEngine;
+namespace MuteOnUnfocus;
+
+[HarmonyPatch(typeof(MainGame), nameof(MainGame.Update))]
+class HarmonyPatch_Focus
+{
+    static void Postfix()
+    {
+        // If application is focused but Audio is paused then unpause audio
+        if (Application.isFocused && AudioListener.pause)
+        {
+            //Console.WriteLine("Focused");
+            AudioListener.pause = false;
+            //Debug.Log("MuteOnUnfocus: Audio Auto Unpaused");
+
+        }
+        // If app is unfocused but Audio is not paused, then pause audio
+        else if (!Application.isFocused && !AudioListener.pause)
+        {
+            //Console.WriteLine("Not Focused");
+            AudioListener.pause = true;
+            //Debug.Log("MuteOnUnfocus: Audio Auto Paused");
+        }
+
+    }
+}
